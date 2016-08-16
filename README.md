c8073cbc677c32941a89a4299e75e9319ad2d02c
mbrunocesar <mbrunocesar@gmail.com>
2016-08-16 20:36:34 -0300

    first patch of refactors and method preparation

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # Further Information Soon
+
+
+## GET Initial Examples
+
+curl -X "GET" http://localhost/user/1
+{"validate":false,"status":"OK","id":"1","name":"Bruno","phone":"11-95287-4246","email":"mbrunocesar@gmail.com"}
+
+curl -X "GET" http://localhost/user/2
+{"validate":false,"status":"OK","id":"2","name":"Cesar","phone":"11-95287-4246","email":"bruno.cesar.paula@usp.br"}
+
+curl -X "GET" http://localhost/user/3
+{"validate":false,"status":"FAIL","reason":"User not found"}
+
+curl -X "GET" http://localhost/user/123
+{"validate":true,"status":"OK","id":"123","name":"Common User","phone":"11 98765-4321","email":"nowhere@noplace.com"}
+
+
+## POST Examples
+
+curl -X POST -F 'name=Cesar' -F 'phone=11-95287-4246' -F 'email=bruno.cesar.paula@usp.br' http://localhost/user/
+{"status":"OK","id":124}
+
+
+## PUT Examples
+
+curl -X PUT -F 'phone=11-91234-8765' http://localhost/user/123
+{"status":"OK","id":123}
+
+
+## DELETE Examples
+
+curl -X "DELETE" http://localhost/user/2
+{"nome":"Cesar","status":"OK"}
+
+curl -X "DELETE" http://localhost/user/2
+{"status":"FAIL","reason":"User not found"}
+
--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -4,23 +4,29 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+
+use AppBundle\Entity\User;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class UserController extends Controller
 {
+
     /**
      * @Route("/users", name="View All Users")
      * @Method("GET")
      */
     public function listAction()
     {
-        // replace this example code with whatever you need
+        $users = $this->userModel()->findAll();
+
         return $this->render('user/list.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+            'users' => $users,
         ]);
     }
+
 
     /**
      * @Route("/user/{id}", name="View User")
@@ -28,11 +34,25 @@ class UserController extends Controller
      */
     public function viewAction($id)
     {
-        // replace this example code with whatever you need
-        return $this->render('user/view.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        $response['validate'] = $this->isValidUser($id);
+
+        $user = $this->userModel()->findOneByIdUser($id);
+        if (isset($user)) {
+            $response['status'] = "OK";
+
+            $response['id'] = $id;
+            $response['name'] = $user->getName();
+            $response['phone'] = $user->getPhone();
+            $response['email'] = $user->getEmail();
+
+        } else {
+            $response['status'] = "FAIL";
+            $response['reason'] = "User not found";
+        }
+
+        return new Response(json_encode($response));
     }
+
 
     /**
      * @Route("/user/{id}", name="Update User")
@@ -40,11 +60,40 @@ class UserController extends Controller
      */
     public function updateAction($id)
     {
-        // replace this example code with whatever you need
-        return $this->render('user/view.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        $em = $this->getDoctrine()->getManager();
+        $user = $em->getRepository('AppBundle:User')->find($id);
+        if (isset($user)) {
+
+            if ($put->get('name')) {
+                $user->setName($put->get('name'));
+            }
+
+            if ($put->get('phone')) {
+                $user->setPhone($put->get('phone'));
+            }
+
+            if ($put->get('email')) {
+                $user->setEmail($put->get('email'));
+            }
+
+            try {
+                $em->flush();
+
+                $response['status'] = "OK";
+
+            } catch (Exception $e) {
+                $response['status'] = "FAIL";
+                $response['reason'] = "DB Error";
+            }
+
+        } else {
+            $response['status'] = "FAIL";
+            $response['reason'] = "User not found";
+        }
+
+        return new Response(json_encode($response));
     }
+
 
     /**
      * @Route("/user/{id}", name="Delete User")
@@ -52,13 +101,93 @@ class UserController extends Controller
      */
     public function deleteAction($id)
     {
+        $user = $this->userModel()->findOneByIdUser($id);
+        if (isset($user)) {
+            $response['nome'] = $user->getName();
 
-        // replace this example code with whatever you need
-        return $this->render('user/view.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+            try {
+                $manager = $this->getDoctrine()->getManager();
+
+                $manager->remove($user);
+                $manager->flush();
+
+                $response['status'] = "OK";
+            } catch (Exception $e) {
+                $response['status'] = "FAIL";
+                $response['reason'] = "DB Error";
+            }
+
+        } else {
+            $response['status'] = "FAIL";
+            $response['reason'] = "User not found";
+        }
+
+        return new Response(json_encode($response));
     }
 
 
+    /**
+     * @Route("/user/", name="Update User")
+     */
+    public function insertAction(Request $post)
+    {
+        $postId = $post->get('id');
+
+        $user = $this->userModel()->findOneByIdUser($postId);
+        if (!isset($user)) {
+            $user = new User();
+
+            if (isset($postId)) {
+                $user->setIdUser($post->get('id'));
+            }
+
+            $user->setName($post->get('name'));
+            $user->setPhone($post->get('phone'));
+            $user->setEmail($post->get('email'));
+
+            try {
+                $em = $this->getDoctrine()->getManager();
+
+                $em->persist($user);
+                $em->flush();
+
+                $response['status'] = "OK";
+                $response['id'] = $user->getIdUser();
+
+            } catch (Exception $e) {
+                $response['status'] = "FAIL";
+                $response['reason'] = "DB Error";
+            }
+
+        } else {
+            $response['status'] = "FAIL";
+            $response['reason'] = "A user with this ID is already found";
+        }
+
+        return new Response(json_encode($response));
+    }
+
+
+    private function userModel() {
+        return $this->getDoctrine()->getRepository('AppBundle:User');
+    }
+
+    private function isValidUser($id) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "http://private-d65c6-phpdevtest.apiary-mock.com/user/{$id}/isvalid");
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        $response = curl_exec($ch);
+
+        try {
+            $data = json_decode($response);
+            $isValid = isset($data) ? $data->validate : false;
+        } catch (Exception $e) {
+            $isValid = false;
+        }
+
+        return $isValid;
+    }
 
 }
